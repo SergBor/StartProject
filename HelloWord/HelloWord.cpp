3bf85dfb51f3a7e6685fb22fd75d21bcb3baf0bb
SergBor <syargey_barautsou@epam.com>
2017-09-04 13:03:41 +0300

    Start

---
+++ HelloWord/HelloWord.cpp
@@ -0,0 +1,13 @@
+// HelloWord.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    std::cout << "Hello Word!\n";
+	return 0;
+}
+
